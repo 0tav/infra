2b47ec1e3091b05dcbea86a67b4f434e3e0a5f1f
0tav <26430271+0tav@users.noreply.github.com>
2026-08-25 20:37:03 +0700

    feat: lock multi-stage with entrypoint and port isolation

---
+++ backend/entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+echo "[ENTRYPOINT] Start checking infrastructure"
+
+DB_HOST="postgres_db"
+DB_PORT="5432"
+
+echo "[ENTRYPOINT] Waiting PostgreSQL at ${DB_HOST}:${DB_PORT} ready to connect"
+
+until nc -z -w 2 "$DB_HOST" "$DB_PORT"; do
+    echo "[ENTRYPOINT] Database not ready yet. Try again after 2 seconds"
+    sleep 2
+done
+
+echo "[ENTRYPOINT] PostgreSQL active and ready to use"
+
+echo "[ENTRYPOINT] Running Prisma Database Migration"
+
+npx prisma migrate deploy --schema=prisma/schema.prisma
+
+echo "[ENTRYPOINT] Database schema successfully updated. Running backend server"
+
+exec "$@"
