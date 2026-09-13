062a1f9f906ad0a105187317f9d46f2dddf8c482
0tav <26430271+0tav@users.noreply.github.com>
2026-09-13 20:55:02 +0700

    fix: secure entrypoint shell text evaluation to prevent early production exit

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,31 +1,29 @@
 #!/bin/sh
 
-set -e
-
 echo "[ENTRYPOINT] Start checking infrastructure"
+
+if [ "${NODE_ENV}" = "production" ] || [ "$NODE_ENV" = "production" ]; then
+    echo "--- Berjalan di Cloud Production: Melewati pengecekan database lokal ---"
+    exec "$@"
+fi
+
+set -e
 
 DB_HOST="postgres_db"
 DB_PORT="5432"
 
-if [ "$NODE_ENV" = "production" ]; then
-  echo "--- Berjalan di Cloud Production: Melewati pengecekan database lokal ---"
-  exec "$@"
-else
+echo "[ENTRYPOINT] Waiting PostgreSQL at ${DB_HOST}:${DB_PORT} ready to connect"
 
-    echo "[ENTRYPOINT] Waiting PostgreSQL at ${DB_HOST}:${DB_PORT} ready to connect"
+until nc -z -w 2 "$DB_HOST" "$DB_PORT"; do
+    echo "[ENTRYPOINT] Database not ready yet. Try again after 2 seconds"
+    sleep 2
+done
 
-    until nc -z -w 2 "$DB_HOST" "$DB_PORT"; do
-        echo "[ENTRYPOINT] Database not ready yet. Try again after 2 seconds"
-        sleep 2
-    done
+echo "[ENTRYPOINT] PostgreSQL active and ready to use"
+echo "[ENTRYPOINT] Running Prisma Database Migration"
 
-    echo "[ENTRYPOINT] PostgreSQL active and ready to use"
+npx prisma migrate deploy --schema=prisma/schema.prisma
 
-    echo "[ENTRYPOINT] Running Prisma Database Migration"
+echo "[ENTRYPOINT] Database schema successfully updated. Running backend server"
 
-    npx prisma migrate deploy --schema=prisma/schema.prisma
-
-    echo "[ENTRYPOINT] Database schema successfully updated. Running backend server"
-
-    exec "$@"
-fi
+exec "$@"
