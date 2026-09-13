ac541d04bbc3db004c18b6d6ac9c91ff977f6a93
0tav <26430271+0tav@users.noreply.github.com>
2026-09-13 20:33:45 +0700

    fix: make entrypoint script smart to bypass local db check in production environment

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -7,19 +7,25 @@ echo "[ENTRYPOINT] Start checking infrastructure"
 DB_HOST="postgres_db"
 DB_PORT="5432"
 
-echo "[ENTRYPOINT] Waiting PostgreSQL at ${DB_HOST}:${DB_PORT} ready to connect"
+if [ "$NODE_ENV" = "production" ]; then
+  echo "--- Berjalan di Cloud Production: Melewati pengecekan database lokal ---"
+  exec "$@"
+else
 
-until nc -z -w 2 "$DB_HOST" "$DB_PORT"; do
-    echo "[ENTRYPOINT] Database not ready yet. Try again after 2 seconds"
-    sleep 2
-done
+    echo "[ENTRYPOINT] Waiting PostgreSQL at ${DB_HOST}:${DB_PORT} ready to connect"
 
-echo "[ENTRYPOINT] PostgreSQL active and ready to use"
+    until nc -z -w 2 "$DB_HOST" "$DB_PORT"; do
+        echo "[ENTRYPOINT] Database not ready yet. Try again after 2 seconds"
+        sleep 2
+    done
 
-echo "[ENTRYPOINT] Running Prisma Database Migration"
+    echo "[ENTRYPOINT] PostgreSQL active and ready to use"
 
-npx prisma migrate deploy --schema=prisma/schema.prisma
+    echo "[ENTRYPOINT] Running Prisma Database Migration"
 
-echo "[ENTRYPOINT] Database schema successfully updated. Running backend server"
+    npx prisma migrate deploy --schema=prisma/schema.prisma
 
-exec "$@"
+    echo "[ENTRYPOINT] Database schema successfully updated. Running backend server"
+
+    exec "$@"
+fi
